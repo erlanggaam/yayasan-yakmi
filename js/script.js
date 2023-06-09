a944136c74c4418d4941c0bb1bf73cff65c02c86
erlanggaam <merlanggamaulana@gmail.com>
2023-06-10 01:11:49 +0700

    yayasan yakmi

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//buat mencet dimana aja dibagian hamburger menu
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
